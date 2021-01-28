247abd6f21f809d99a88fedaacab7fa33cb6dcd4
CherryBoy99 <70683673+CherryBoy99@users.noreply.github.com>
2021-01-28 11:13:57 +0200

    Add files via upload

---
+++ Catsanddogs/app/script.js
@@ -0,0 +1,5 @@
+//cats API - https://api.thecatapi.com/v1/images/search
+//dog API - hhtp://dog.ceo/api/breeds/image/random
+
+    const cat_btn = document.querySelector("#cat_btn");
+    const cat_result = document.querySelector("#cat_result")
